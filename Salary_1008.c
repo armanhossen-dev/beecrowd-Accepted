17c0dc2a0618ca5a13adc76169bdd5b9ef2a4564
Arman Hossen <145398391+1ahrn@users.noreply.github.com>
2025-01-27 04:23:21 +0600

    cp

--- Salary_1008.c
+++ Salary_1008.c
@@ -2,17 +2,11 @@
  
 int main() {
  
-    int num,sal;
-    
-    float wh,SALARY;
-    
+    int num,sal;    
+    float wh,SALARY;    
     scanf("%d%d%f",&num,&sal,&wh);
-    
     SALARY = sal * wh ; 
-    
     printf("NUMBER = %d\n",num);
-    
     printf("SALARY = U$ %.2f\n",SALARY);
- 
     return 0;
 }
